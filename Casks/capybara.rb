6e1f22d9880f3d715b45099dc114efe7b57c829b
Son Tran <ttsalpha@icloud.com>
2025-10-29 18:19:59 +0100

    add Capybara cask

---
+++ Casks/capybara.rb
@@ -0,0 +1,24 @@
+cask "capybara" do
+  version "1.0.0"
+  sha256 "ae6da4d7e355a8609e55babaf9e0119ff039280670f7067a864eac2fc3d7641a"
+
+  url "https://github.com/ttsalpha/Capybara/releases/download/v#{version}/Capybara.dmg"
+  name "Capybara"
+  desc "A compact toolbox simplifying developer data tasks."
+  homepage "https://capybara.ttsalpha.com"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Capybara.app"
+
+  uninstall delete: "/Applications/Capybara.app"
+
+  zap trash: [
+    "~/Library/Application Support/Capybara",
+    "~/Library/Preferences/com.ttsalpha.capybara.plist",
+    "~/Library/Containers/com.ttsalpha.Capybara",
+  ]
+end
