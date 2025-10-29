f2addede56b3016a8f90cf1337188d0ef4200795
Son Tran <ttsalpha@icloud.com>
2025-10-29 18:46:25 +0100

    capybara: add verified domain and macOS dependency

--- Casks/capybara.rb
+++ Casks/capybara.rb
@@ -2,15 +2,18 @@ cask "capybara" do
   version "1.0.0"
   sha256 "ae6da4d7e355a8609e55babaf9e0119ff039280670f7067a864eac2fc3d7641a"
 
-  url "https://github.com/ttsalpha/Capybara/releases/download/v#{version}/Capybara.dmg"
+  url "https://github.com/ttsalpha/Capybara/releases/download/v#{version}/Capybara.dmg",
+      verified: "github.com/ttsalpha/"
   name "Capybara"
-  desc "A compact toolbox simplifying developer data tasks."
-  homepage "https://capybara.ttsalpha.com"
+  desc "Compact toolbox simplifying developer data tasks"
+  homepage "https://capybara.ttsalpha.com/"
 
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  depends_on macos: ">= :sonoma"
 
   app "Capybara.app"
 
@@ -18,7 +21,7 @@ cask "capybara" do
 
   zap trash: [
     "~/Library/Application Support/Capybara",
-    "~/Library/Preferences/com.ttsalpha.capybara.plist",
     "~/Library/Containers/com.ttsalpha.Capybara",
+    "~/Library/Preferences/com.ttsalpha.capybara.plist",
   ]
 end
